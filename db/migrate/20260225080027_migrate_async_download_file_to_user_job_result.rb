103983a43fac7bc59d5e4243f96487b29cdbd774
Jannik Pulfer <pulfer@puzzle.ch>
2026-03-05 12:19:15 +0100

    Add missing license headers

--- db/migrate/20260225080027_migrate_async_download_file_to_user_job_result.rb
+++ db/migrate/20260225080027_migrate_async_download_file_to_user_job_result.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2012-2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
 class MigrateAsyncDownloadFileToUserJobResult < ActiveRecord::Migration[8.0]
   def change
     rename_table :async_download_files, :user_job_results
